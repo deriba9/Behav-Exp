52d85a2d1b68d67be1c208fb5232472c7cadd9cb
deriba9 <epic86.agm@gmail.com>
2023-01-22 22:15:39 +0900

    first commit

---
+++ database_config.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "mysql8029.xserver.jp";
+  $port = 3306;
+  $username = "brainstudy_wp1";
+  $password = "44txl4dz7h";
+  $dbname = "brainstudy_wp1";
+  $table = "mytable";
+?>
